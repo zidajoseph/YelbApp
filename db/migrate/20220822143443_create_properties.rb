5232b21247229ff9e4ed660e9f57d724b3f9f57e
zidajoseph <zidajoseph60@gmail.com>
2022-08-22 14:56:00 +0000

    Adding Property model and gem faker

---
+++ db/migrate/20220822143443_create_properties.rb
@@ -0,0 +1,19 @@
+class CreateProperties < ActiveRecord::Migration[6.0]
+  def change
+    create_table :properties do |t|
+      t.string :name
+      t.string :headline
+      t.text :description
+      t.string :city
+      t.string :state
+      t.string :country
+      t.string :address_1
+      t.string :address_2
+      t.string :latitude
+      t.string :longitude
+      t.string :zip_code
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_22_090401) do
+ActiveRecord::Schema.define(version: 2022_08_22_143443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "properties", force: :cascade do |t|
+    t.string "name"
+    t.string "headline"
+    t.text "description"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.string "address_1"
+    t.string "address_2"
+    t.string "latitude"
+    t.string "longitude"
+    t.string "zip_code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
