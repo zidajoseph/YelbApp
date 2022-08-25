e78bcf31e0587289d404d27a9880a0a8fd3a6218
zidajoseph <zidajoseph60@gmail.com>
2022-08-25 09:47:04 +0000

    adding admin and partner devise, setup acces

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :partners
+  devise_for :admins
   resources :properties
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
